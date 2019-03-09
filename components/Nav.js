653bddb04d22a02751207af6685fae757bb1addf
Mike Bannister <notimpossiblemike@gmail.com>
2019-03-08 21:20:11 -0500

    Add instructions

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,19 @@
 import Link from 'next/link'
 
 export default () => (
-  <nav>
-    <style jsx>{`
-      nav {
-        position: absolute;
-        right: 10px;
-        top: 10px;
-      }
-    `}</style>
-    <Link href='/'><a>home</a></Link>
-    {` `}|{` `}
-    <Link href='/about'><a>about</a></Link>
-  </nav>
+  <>
+    <nav>
+      <style jsx>{`
+        nav {
+          position: absolute;
+          right: 10px;
+          top: 10px;
+        }
+      `}</style>
+      <Link href='/'><a>home</a></Link>
+      {` `}|{` `}
+      <Link href='/about'><a>about</a></Link>
+    </nav>
+    <p><em>note: to see flicker choose "yes" and refresh page</em></p>
+  </>
 )
